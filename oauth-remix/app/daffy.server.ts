05b321c2d641fef8b335b208321195c7f503f667
Sergio Xalambrí <hello@sergiodxa.com>
2023-05-22 23:58:27 -0500

    Remove /public/api from API requests

--- oauth-remix/app/daffy.server.ts
+++ oauth-remix/app/daffy.server.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const BASE_URL = new URL("https://public.daffy.org/");
 
 export async function fetchUser(token: string) {
-  let response = await fetch(new URL("/public/api/v1/users/me", BASE_URL), {
+  let response = await fetch(new URL("/v1/users/me", BASE_URL), {
     headers: { Authorization: token },
   });
 
