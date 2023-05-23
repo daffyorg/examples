23b265c459f8dc266cfd13d45d6ef79f1802a35d
Sergio Xalambrí <79724065+sergiodxa-silverback@users.noreply.github.com>
2023-05-23 18:11:08 -0500

    Add campaign example app (#11)
    * Add campaign example app
    
    * Update auth.server.ts
    
    * Delete entry files
    
    * Create PUBLIC_HOST env variable
    
    * Remove /public/api from API urls
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Sergio Xalambrí <hello@sergiodxa.com>

---
+++ campaign/README.md
@@ -0,0 +1,23 @@
+# Campaign App
+
+This is a Remix example application that uses a combination of Daffy's personal API keys and OAuth2 client to create a campaign to get funds for a specific non profit.
+
+The campaing uses an EIN defined as an environment variable to fetch the non profit information from the Daffy's API using the personal API key.
+
+Then uses OAuth2 to allow end-users to authenticate and create a $18 donations to that specific non profit.
+
+## Prerequisites
+
+1. Node.js (v14.0 or higher)
+2. NPM (v6.0 or higher)
+3. A Daffy client ID and secret
+4. A Daffy personal API key (https://www.daffy.org/settings/api)
+
+> **Note** to get a client ID and secret contact us at partner@daffy.org
+
+## Setup
+
+1. Clone the repository and navigate to the project folder.
+2. Install the dependencies with `npm install`.
+3. Rename the `.env.template` to `.env` and put your client ID and secret, your personal API key and a non profit EIN.
+4. Run the application with `npm run dev`
---
+++ campaign/app/daffy.server.ts
@@ -0,0 +1,49 @@
+import { z } from "zod";
+
+let BASE_URL = new URL("https://public.daffy.org/");
+
+export async function fetchUser(token: string) {
+  let response = await fetch(new URL("/v1/users/me", BASE_URL), {
+    headers: { Authorization: token },
+  });
+
+  if (!response.ok) return null;
+
+  let body = await response.json();
+
+  return z.object({ id: z.number(), name: z.string() }).parse(body);
+}
+
+export async function fetchCharity(token: string, ein: string) {
+  let response = await fetch(new URL(`/v1/non_profits/${ein}`, BASE_URL), {
+    headers: { "X-Api-Key": token },
+  });
+
+  if (!response.ok) throw new Error("Charity not found");
+
+  let body = await response.json();
+
+  return z.object({ name: z.string() }).parse(body);
+}
+
+export async function fetchDonate(token: string, ein: string, amount: number) {
+  let user = await fetchUser(token);
+  if (!user) throw new Error("User not found");
+
+  let response = await fetch(
+    new URL(`/v1/users/${user.id}/donations`, BASE_URL),
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json", Authorization: token },
+      body: JSON.stringify({ amount, ein }),
+    }
+  );
+
+  if (!response.ok) throw new Error("Failed to donate");
+
+  let body = await response.json();
+
+  return z
+    .object({ id: z.number(), amount: z.number(), status: z.string() })
+    .parse(body);
+}
